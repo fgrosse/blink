8d8a3182e30c1e7bd73832d915e07b2613efc1c0
Friedrich Große <friedrich.grosse@gmail.com>
2015-12-28 17:53:28 +0100

    Fix broken tests

--- command_test.go
+++ command_test.go
@@ -8,15 +8,15 @@ import (
 )
 
 func TestSetRGBCommand(t *testing.T) {
-	c := setRGBCommand{rgb{2, 3, 4}}
+	c := setRGBCommand{Color{2, 3, 4}}
 	assert.Equal(t, []byte{0x01, 'n', 0x02, 0x03, 0x04, 0x00, 0x00, 0x00}, c.bytes())
 }
 
 func TestFadeRGBCommand(t *testing.T) {
-	c := fadeRGBCommand{rgb: rgb{2, 3, 4}, duration: 328010 * time.Millisecond, n: 1}
+	c := fadeRGBCommand{Color: Color{2, 3, 4}, duration: 328010 * time.Millisecond, n: 1}
 	assert.Equal(t, []byte{0x01, 'c', 0x02, 0x03, 0x04, 0x80, 0x21, 0x01}, c.bytes())
 
-	c = fadeRGBCommand{rgb: rgb{255, 255, 255}, duration: 5 * time.Second}
+	c = fadeRGBCommand{Color: Color{255, 255, 255}, duration: 5 * time.Second}
 	assert.Equal(t, []byte{0x01, 'c', 0xff, 0xff, 0xff, 0x01, 0xf4, 0x00}, c.bytes())
 
 }
